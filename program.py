73821e182ac59e37a83f5a897727e903d360b4f0
John Ric Merque <johnricmer@gmail.com>
2022-11-19 10:30:03 +0800

    imported request module and used get request method for URL

--- program.py
+++ program.py
@@ -6,9 +6,14 @@
 # install request module
 # initialize API key and Base URL
 
+import requests
+
 API_KEY = "4d7436b2e953a39a0d36c842e7742e02"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-#asks user input of city to integrate into the URL with API 
+# asks user input of city to integrate into the URL with API 
 city_name = input("What city do you want to look into?")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city_name}"
+
+# request module method to get request url
+response = requests.get(request_url)
