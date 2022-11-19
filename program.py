7d5e92dd44c16f6c681a7c268f3e1201b1cbc3c1
John Ric Merque <johnricmer@gmail.com>
2022-11-19 13:07:49 +0800

    added color scheme

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 # John Ric Merque | BSCOE 2-6 | Weather Fetcher using Python and weather API
-# This program is referenced to YT tutorial Video: https://www.youtube.com/watch?v=Oz3W-LKfafE&ab_channel=TechWithTim. I made some slight modifications to improve the program.
+# This program is referenced to YT tutorial Video: https://www.youtube.com/watch?v=Oz3W-LKfafE&ab_channel=TechWithTim. 
+# I made some slight modifications to improve the program.
 
 # Pseudocode
 # get weather data from API
@@ -13,7 +14,7 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 while True: 
     # asks user input of city to integrate into the URL with API
-    print("\nWEATHER FETCHER") 
+    print("\n\033[46mWEATHER FETCHER\033[0m") 
     city_name = input("\nWhich city do you want to look into?: ")
     request_url = f"{BASE_URL}?appid={API_KEY}&q={city_name}"
 
@@ -33,12 +34,12 @@ while True:
 
         # display
         print("=============================================================")
-        print("We found your city!\n") 
+        print("\033[33mWe found your city!\n") 
         print(f"{city_name.title()}, {country}: is having {weather}.")
         print(f"The current temperature in the city is {temperature}°C.")
         print(f"But it would feel like {feels_like}°C.")
         print(f"While the current wind speed in the area is {wind_speed}m/s.")
-        print(f"\nThat's all, Stay Safe!")
+        print(f"\nThat's all, Stay Safe!\033[0m")
         print("=============================================================") 
 
     else:
