a5e0b59207e3e80d350d0db35e61d005d8a4e68f
John Ric Merque <johnricmer@gmail.com>
2022-11-19 10:11:39 +0800

    initialized first part pseudocode, declared API key and base URL

---
+++ program.py
@@ -0,0 +1,10 @@
+# Weather Fetcher using Python and weather API
+# YT tutorial Video: https://www.youtube.com/watch?v=Oz3W-LKfafE&ab_channel=TechWithTim
+
+# Pseudocode
+# get weather data from API
+# install request module
+# initialize API key and Base URL
+
+API_KEY = "4d7436b2e953a39a0d36c842e7742e02"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
